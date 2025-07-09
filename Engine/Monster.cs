7615256ad6e821328934c69ed7f1abf00a71c20f
MilanHennessy <milanhennessy@gmail.com>
2025-07-08 21:09:46 -0400

    created more classes

---
+++ Engine/Monster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class Monster
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaxHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+        public int MaxDmg { get; set; }
+        public int RewardExpPoints { get; set; }
+        public int RewardGold { get; set; }
+
+    }
+}
